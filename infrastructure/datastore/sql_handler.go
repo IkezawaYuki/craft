8f31ee57518692f6f2219e013723dec3cebae8d7
yikezawa <y.ikezawa93@gmail.com>
2020-06-27 01:04:31 +0900

    mod

--- infrastructure/datastore/sql_handler.go
+++ infrastructure/datastore/sql_handler.go
@@ -1,1 +1,28 @@
 package datastore
+
+import (
+	"IkezawaYuki/craft/config"
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+const (
+	tableNameSignalEvents = "signal_events"
+)
+
+func Connect() *sql.DB {
+	var err error
+	db, err = sql.Open(config.ConfigList.SQLDriver, config.ConfigList.DbName)
+	if err != nil {
+		panic(err)
+	}
+	cmd := fmt.Sprintf(`
+		CREATE TABLE IF NOT
+	`)
+	db.Exec(cmd)
+
+	return db
+}
